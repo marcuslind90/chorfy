e0b87739397887d7827086281fab745b49a53c12
Marcus Lind <marcuslind90@gmail.com>
2018-11-01 20:31:12 +0700

    Changed permission and structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Build Status](https://travis-ci.com/marcuslind90/chorfy.svg?branch=master)](https://travis-ci.com/marcuslind90/chorfy)
+
 # Chorfy
 Chorfy is an anchor news application that aggregate news from multiple sources.
--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # noqa
 
 
 # Quick-start development settings - unsuitable for production
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'chorfy.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'chorfy.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
